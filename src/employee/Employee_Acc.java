89c9e77655b3808914742af1b72a2a26432a90ed
Zaheeraz <zaheeraz730@gmail.com>
2023-09-05 19:35:09 +0530

    added syso

--- src/employee/Employee_Acc.java
+++ src/employee/Employee_Acc.java
@@ -4,6 +4,7 @@ public class Employee_Acc {
 
 	public static void main(String[] args) {
 		 String str = "zaheer";
+		 System.out.println(str);
 		
           System.out.println("employee Details");
 	}
