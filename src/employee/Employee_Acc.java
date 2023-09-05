7aee18f0efc220e732e5c40c348319d1afccc76b
Zaheeraz <142376884+Zaheeraz@users.noreply.github.com>
2023-09-05 19:20:34 +0530

    Update Employee_Acc.java
    added string datatype

--- src/employee/Employee_Acc.java
+++ src/employee/Employee_Acc.java
@@ -3,7 +3,8 @@ package employee;
 public class Employee_Acc {
 
 	public static void main(String[] args) {
-		 
+		 String str = "zaheer";
+		
           System.out.println("employee Details");
 	}
 
